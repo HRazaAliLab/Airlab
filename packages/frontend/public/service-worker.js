c34dd06402e07590d0edb8ad20bf857e3fc10650
Anton Rau <anton.rau@gmail.com>
2020-09-18 15:35:27 +0200

    New Reactivity sub-table for Species view.

--- packages/frontend/public/service-worker.js
+++ packages/frontend/public/service-worker.js
@@ -7,7 +7,7 @@
  */
 
 //Change this value every time before you build
-const LATEST_VERSION = "2020.09.17";
+const LATEST_VERSION = "2020.09.18";
 
 workbox.core.setCacheNameDetails({ prefix: "airlab" });
 
--- packages/frontend/src/views/main/group/species/SpeciesExpandedView.vue
+++ packages/frontend/src/views/main/group/species/SpeciesExpandedView.vue
@@ -1,64 +1,126 @@
 <template>
-  <v-card flat class="my-2">
-    <v-card-text class="text-subtitle-2">Clones</v-card-text>
-    <v-data-table
-      :headers="headers"
-      :items="items"
-      :loading="loading"
-      :items-per-page="-1"
-      hide-default-footer
-      disable-filtering
-      dense
-    >
-      <template v-slot:item.id="{ item }">
-        <router-link
-          class="link"
-          :to="{
-            name: 'main-group-clones-edit',
-            params: {
-              groupId: activeGroupId,
-              id: item.id,
-            },
-          }"
-        >
-          {{ item.id }}
-        </router-link>
-      </template>
-      <template v-slot:item.protein="{ item }">
-        <router-link
-          v-if="item.protein"
-          class="link"
-          :to="{
-            name: 'main-group-proteins-edit',
-            params: {
-              groupId: activeGroupId,
-              id: item.protein.id,
-            },
-          }"
-        >
-          {{ item.protein.name }}
-        </router-link>
-      </template>
-      <template v-slot:item.isPhospho="{ item }">
-        <v-icon v-if="item.isPhospho">mdi-check</v-icon>
-      </template>
-      <template v-slot:item.isPolyclonal="{ item }">
-        <v-icon v-if="item.isPolyclonal">mdi-check</v-icon>
-      </template>
-      <template v-slot:item.validations="{ item }">
-        <v-chip
-          v-for="validation in item.validations"
-          :key="validation.id"
-          :color="getStatusColor(validation)"
-          class="mr-1"
-          x-small
-          dark
-        >
-          {{ validation.application | applicationToString }}
-        </v-chip>
-      </template>
-    </v-data-table>
-  </v-card>
+  <v-tabs v-model="tab" class="my-1">
+    <v-tab>Hosts</v-tab>
+    <v-tab>Reactivity</v-tab>
+    <v-tab-item>
+      <v-data-table
+        :headers="headers"
+        :items="hostItems"
+        :loading="loading"
+        :items-per-page="-1"
+        hide-default-footer
+        disable-filtering
+        dense
+      >
+        <template v-slot:item.id="{ item }">
+          <router-link
+            class="link"
+            :to="{
+              name: 'main-group-clones-edit',
+              params: {
+                groupId: activeGroupId,
+                id: item.id,
+              },
+            }"
+          >
+            {{ item.id }}
+          </router-link>
+        </template>
+        <template v-slot:item.protein="{ item }">
+          <router-link
+            v-if="item.protein"
+            class="link"
+            :to="{
+              name: 'main-group-proteins-edit',
+              params: {
+                groupId: activeGroupId,
+                id: item.protein.id,
+              },
+            }"
+          >
+            {{ item.protein.name }}
+          </router-link>
+        </template>
+        <template v-slot:item.isPhospho="{ item }">
+          <v-icon v-if="item.isPhospho">mdi-check</v-icon>
+        </template>
+        <template v-slot:item.isPolyclonal="{ item }">
+          <v-icon v-if="item.isPolyclonal">mdi-check</v-icon>
+        </template>
+        <template v-slot:item.validations="{ item }">
+          <v-chip
+            v-for="validation in item.validations"
+            :key="validation.id"
+            :color="getStatusColor(validation)"
+            class="mr-1"
+            x-small
+            dark
+          >
+            {{ validation.application | applicationToString }}
+          </v-chip>
+        </template>
+      </v-data-table>
+    </v-tab-item>
+    <v-tab-item>
+      <v-data-table
+        :headers="headers"
+        :items="reactivityItems"
+        :loading="loading"
+        :items-per-page="-1"
+        hide-default-footer
+        disable-filtering
+        dense
+      >
+        <template v-slot:item.id="{ item }">
+          <router-link
+            class="link"
+            :to="{
+              name: 'main-group-clones-edit',
+              params: {
+                groupId: activeGroupId,
+                id: item.id,
+              },
+            }"
+          >
+            {{ item.id }}
+          </router-link>
+        </template>
+        <template v-slot:item.protein="{ item }">
+          <router-link
+            v-if="item.protein"
+            class="link"
+            :to="{
+              name: 'main-group-proteins-edit',
+              params: {
+                groupId: activeGroupId,
+                id: item.protein.id,
+              },
+            }"
+          >
+            {{ item.protein.name }}
+          </router-link>
+        </template>
+        <template v-slot:item.isPhospho="{ item }">
+          <v-icon v-if="item.isPhospho">mdi-check</v-icon>
+        </template>
+        <template v-slot:item.isPolyclonal="{ item }">
+          <v-icon v-if="item.isPolyclonal">mdi-check</v-icon>
+        </template>
+        <template v-slot:item.validations="{ item }">
+          <v-chip
+            v-for="validation in item.validations"
+            :key="validation.id"
+            :color="getStatusColor(validation)"
+            class="mr-1"
+            x-small
+            dark
+          >
+            {{ validation.application | applicationToString }}
+          </v-chip>
+        </template>
+      </v-data-table>
+    </v-tab-item>
+  </v-tabs>
 </template>
 
 <script lang="ts">
@@ -68,10 +130,12 @@ import { groupModule } from "@/modules/group";
 import { SpeciesDto } from "@airlab/shared/lib/species/dto";
 import { speciesModule } from "@/modules/species";
 import { getStatusColor } from "@/utils/converters";
+import { cloneModule } from "@/modules/clone";
 
 @Component
 export default class SpeciesExpandedView extends Vue {
   private readonly groupContext = groupModule.context(this.$store);
+  private readonly cloneContext = cloneModule.context(this.$store);
   private readonly speciesContext = speciesModule.context(this.$store);
 
   private readonly getStatusColor = getStatusColor;
@@ -116,15 +180,32 @@ export default class SpeciesExpandedView extends Vue {
     },
   ];
 
+  private tab = 0;
   private loading = true;
-  private items: CloneDto[] = [];
+  private hostItems: CloneDto[] = [];
 
   private get activeGroupId() {
     return this.groupContext.getters.activeGroupId;
   }
 
+  private get clones() {
+    return this.cloneContext.getters.clones;
+  }
+
+  private get reactivityItems() {
+    return this.clones.filter((item) => {
+      if (item.reactivity.includes(this.species.id)) {
+        return item;
+      }
+    });
+  }
+
   async mounted() {
-    this.items = await this.speciesContext.actions.getSpeciesClones(this.species.id);
+    const result = await Promise.all([
+      this.speciesContext.actions.getSpeciesClones(this.species.id),
+      this.cloneContext.actions.getGroupClones(this.activeGroupId!),
+    ]);
+    this.hostItems = result[0];
     this.loading = false;
   }
 }
