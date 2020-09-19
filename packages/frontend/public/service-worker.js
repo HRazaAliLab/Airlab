f30b003a43e6a7abedf555fcdcaca20948f69167
Anton Rau <anton.rau@gmail.com>
2020-09-19 23:43:58 +0200

    WIP: airlab.bio domain integration

--- packages/frontend/public/service-worker.js
+++ packages/frontend/public/service-worker.js
@@ -7,7 +7,7 @@
  */
 
 //Change this value every time before you build
-const LATEST_VERSION = "2020.09.18";
+const LATEST_VERSION = "2020.09.19";
 
 workbox.core.setCacheNameDetails({ prefix: "airlab" });
 
