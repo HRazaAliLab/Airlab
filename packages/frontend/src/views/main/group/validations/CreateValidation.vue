31a42672d5f75be31ff5774ed3cee062edcc5640
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2025-09-25 14:32:02 +0100

    Try calling the function.

--- packages/frontend/src/views/main/group/validations/CreateValidation.vue
+++ packages/frontend/src/views/main/group/validations/CreateValidation.vue
@@ -54,7 +54,7 @@
                 v-model="lotId"
                 :search-input.sync="lotSearchInput"
                 :items="lots"
-                item-text="nameAndNumber"
+                item-text="nameAndNumber()"
                 item-value="id"
                 clearable
                 open-on-clear
