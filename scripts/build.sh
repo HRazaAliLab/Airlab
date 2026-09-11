cadd68111e17b73045f09c0eac23f09c0feb4bf6
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2026-09-11 14:21:05 +0100

    Removed parallel image building.

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,4 +8,4 @@ source /etc/airlab.sh
 NODE_ENV=production \
 docker compose -f .deploy/build.yml config > docker-stack.yml
 
-docker compose -f docker-stack.yml build --parallel
+docker compose -f docker-stack.yml build
